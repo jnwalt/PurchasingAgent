723affa0c8d2c70d359446b7d9df1461d9370a7e
leetai <ltdry@163.com>
2016-02-25 22:51:59 +0800

    123

--- app/src/main/java/com/leetai/purchasingagent/test/TestActivity.java
+++ app/src/main/java/com/leetai/purchasingagent/test/TestActivity.java
@@ -2,12 +2,15 @@ package com.leetai.purchasingagent.test;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -108,59 +111,65 @@ public class TestActivity extends Activity {
                 return;
             } else {
                 Uri uri = data.getData();
-                Log.i("uri:", uri.toString());
-                try {
 
-                    Bitmap bitmap = getPicFromBytes(null, uri);
-                    im.setImageBitmap(bitmap);
-                    Uri uri1 = saveBitmap(bitmap);
-                   // Log.i("uri1:", uri1.toString());
-//                    Bitmap bitmap =   MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                im.setImageURI(uri);
+                String path1 = getImageAbsolutePath(TestActivity.this, uri);
+                Log.i("path1:", path1);
+
+                Bitmap bitmap = BitmapFactory.decodeFile(path1);
+                // im.setImageBitmap(bitmap);
+//                try {
+//
+//                    Bitmap bitmap = getPicFromBytes(null, uri);
 //                    im.setImageBitmap(bitmap);
-
-//                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-//                    byte[] bytes = baos.toByteArray();
-
-
-                    String path = "/sdcard/pa/test.png";
-                    String url = HttpTool.getUrl("", "UpLoadServlet");
-//                     InputStream  InputStream = new FileInputStream(path);
-                    RequestParams params = new RequestParams();
-                    params.addBodyParameter("param", new File(path));
-                    HttpUtils http = new HttpUtils();
-
-                    http.send(HttpRequest.HttpMethod.POST, url, params,
-                            new RequestCallBack<String>() {
-                                @Override
-                                public void onStart() {
-                                    tv_test.setText("conn...");
-                                }
-
-                                @Override
-                                public void onLoading(long total, long current, boolean isUploading) {
-                                    if (isUploading) {
-                                        tv_test.setText("upload: " + current + "/" + total);
-                                    } else {
-                                        tv_test.setText("reply: " + current + "/" + total);
-                                    }
-                                }
-
-                                @Override
-                                public void onSuccess(ResponseInfo<String> responseInfo) {
-                                    tv_test.setText("reply: " + responseInfo.result);
-                                }
-
-                                @Override
-                                public void onFailure(HttpException error, String msg) {
-                                    tv_test.setText(error.getExceptionCode() + ":" + msg);
-                                }
-                            });
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+//                    Uri uri1 = saveBitmap(bitmap);
+//                   // Log.i("uri1:", uri1.toString());
+////                    Bitmap bitmap =   MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+////                    im.setImageBitmap(bitmap);
+//
+////                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+////                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+////                    byte[] bytes = baos.toByteArray();
+//
+//
+//                    String path = "/sdcard/pa/test.png";
+//                    String url = HttpTool.getUrl("", "UpLoadServlet");
+////                     InputStream  InputStream = new FileInputStream(path);
+//                    RequestParams params = new RequestParams();
+//                    params.addBodyParameter("param", new File(path));
+//                    HttpUtils http = new HttpUtils();
+//
+//                    http.send(HttpRequest.HttpMethod.POST, url, params,
+//                            new RequestCallBack<String>() {
+//                                @Override
+//                                public void onStart() {
+//                                    tv_test.setText("conn...");
+//                                }
+//
+//                                @Override
+//                                public void onLoading(long total, long current, boolean isUploading) {
+//                                    if (isUploading) {
+//                                        tv_test.setText("upload: " + current + "/" + total);
+//                                    } else {
+//                                        tv_test.setText("reply: " + current + "/" + total);
+//                                    }
+//                                }
+//
+//                                @Override
+//                                public void onSuccess(ResponseInfo<String> responseInfo) {
+//                                    tv_test.setText("reply: " + responseInfo.result);
+//                                }
+//
+//                                @Override
+//                                public void onFailure(HttpException error, String msg) {
+//                                    tv_test.setText(error.getExceptionCode() + ":" + msg);
+//                                }
+//                            });
+//
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
 
             }
         }
@@ -236,5 +245,99 @@ public class TestActivity extends Activity {
 
         }
         return Uri.fromFile(png);
+    }
+
+    public static String getImageAbsolutePath(Activity context, Uri imageUri) {
+        if (context == null || imageUri == null)
+            return null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, imageUri)) {
+            if (isExternalStorageDocument(imageUri)) {
+                String docId = DocumentsContract.getDocumentId(imageUri);
+                String[] split = docId.split(":");
+                String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(imageUri)) {
+                String id = DocumentsContract.getDocumentId(imageUri);
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(imageUri)) {
+                String docId = DocumentsContract.getDocumentId(imageUri);
+                String[] split = docId.split(":");
+                String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                String selection = MediaStore.Images.Media._ID + "=?";
+                String[] selectionArgs = new String[]{split[1]};
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        } // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(imageUri.getScheme())) {
+            // Return the remote address
+            if (isGooglePhotosUri(imageUri))
+                return imageUri.getLastPathSegment();
+            return getDataColumn(context, imageUri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(imageUri.getScheme())) {
+            return imageUri.getPath();
+        }
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        String column = MediaStore.Images.Media.DATA;
+        String[] projection = {column};
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 }
